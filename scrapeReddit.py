b6735a85235bdc636ec5c1e7415d6548e2ccf097
Rohan Agarwal <ro.agarwal@hotmail.com>
2021-02-05 20:20:21 -0500

    Collect all data and put in dictionaries

--- scrapeReddit.py
+++ scrapeReddit.py
@@ -3,10 +3,30 @@ import praw
 
 reddit = praw.Reddit(client_id='7pdHgJ0aNnIqkQ', client_secret='QU-vPCVM1dAO3beUcrIghrHraRoULA', user_agent='my_user_agent')
 
-posts = []
+def extract_comments(post):
+    comment_text = ""
+    post.comments.replace_more(limit=0)
+    for top_level_comment in post.comments:
+        comment_text += " " + top_level_comment.body
+    return comment_text
 
-# get 10 hot posts from the WSB subreddit
-hot_posts = reddit.subreddit('WallStreetBets').hot(limit=10)
-for post in hot_posts:
-    print(post.title)
-    posts.append({})
+def scrape():
+    posts = []
+
+    # get 10 hot posts from the WSB subreddit
+    hot_posts = reddit.subreddit('WallStreetBets').hot(limit=25)
+    for post in hot_posts:
+        posts.append({
+            "title": post.title, 
+            "score": post.score, 
+            "id": post.id, 
+            "subreddit": post.subreddit, 
+            "url": post.url, 
+            "num_comments": post.num_comments, 
+            "selftext": post.selftext,
+            "created": post.created,
+            "comments": extract_comments(post)
+        })
+
+
+scrape()
