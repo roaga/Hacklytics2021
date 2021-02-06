ba920d8dbec907d2a8ec4c6c84e28a9029973607
Rohan Agarwal <ro.agarwal@hotmail.com>
2021-02-05 20:42:16 -0500

    Separate comment text and add basic weighting

--- scrapeReddit.py
+++ scrapeReddit.py
@@ -4,11 +4,20 @@ import praw
 reddit = praw.Reddit(client_id='7pdHgJ0aNnIqkQ', client_secret='QU-vPCVM1dAO3beUcrIghrHraRoULA', user_agent='my_user_agent')
 
 def extract_comments(post):
-    comment_text = ""
+    comment_text = []
     post.comments.replace_more(limit=8)
     for top_level_comment in post.comments:
-        comment_text += " " + top_level_comment.body
+        comment_text.append(top_level_comment.body)
     return comment_text
+
+def weigh(post_data):
+    score = post_data["score"]
+    created = post_data["created"]
+    num_comments = post_data["num_comments"]
+    polarity = post_data["polarity"] #TODO: check for field name
+
+    impact = score + created / 100000 + num_comments * 10 + abs(2 - polarity) * 100
+    return impact
 
 def scrape():
     posts = []
@@ -32,4 +41,10 @@ def scrape():
 
 
 data = scrape()
-print(data)
+weights = []
+
+for i, item in enumerate(data):
+    weight = weigh(item)
+    weights.append({item["id"]: weight})
+
+print(weights)
